e1927ff8e189c7a4a2bc8da6e0877c2110b141d2
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2024-11-22 21:51:52 +0530

    Deployement-1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/parkSmart/',
+  base: '/parkSmart/',  // This sets the base path for your app on GitHub Pages
+  build: {
+    outDir: 'dist',  // Ensure the build output is in the 'dist' folder
+  },
 });
