eb81cb5ccdcd826ed4c55d21a1d382204ee6fa93
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-03-14 14:01:46 +0530

    fix-2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/parkSmart/',  // This sets the base path for your app on GitHub Pages
+  base: '/ParkSmart/',  // This sets the base path for your app on GitHub Pages
   build: {
     outDir: 'dist',  // Ensure the build output is in the 'dist' folder
   },
